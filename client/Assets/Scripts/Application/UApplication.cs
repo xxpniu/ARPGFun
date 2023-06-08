2e2760889d5e3bbf33480c539f622f8891a53301
andrew <onedot@vip.qq.com>
2023-06-08 10:30:55 +0800

    fix

--- client/Assets/Scripts/Application/UApplication.cs
+++ client/Assets/Scripts/Application/UApplication.cs
@@ -126,7 +126,7 @@ public class UApplication : XSingleton<UApplication>
         {
             // Init(args[0] as DHero, args[1] as PlayerPackage, (int)args[2]);
             var config = ExcelToJSONConfigManager.First<CharacterData>(t => t.ID > 0);
-            var itemCfg = ExcelToJSONConfigManager.GetId<ItemData>(config.InitEquip);
+            var itemCfg = ExcelToJSONConfigManager.GetId<ItemData>(int.Parse(config.InitEquip));
             var item = new PlayerItem()
             {
                 ItemID = itemCfg.ID,
