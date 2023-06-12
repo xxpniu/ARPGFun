63a53e8e0ab3929a42beb826efd766c2710ab39e
andrew <onedot@vip.qq.com>
2023-06-12 11:01:57 +0800

    fix

--- client/Assets/Scripts/UApp/Utility/ExtendTools.cs
+++ client/Assets/Scripts/UApp/Utility/ExtendTools.cs
@@ -15,54 +15,51 @@ public static class ExtendTools
 
     static ExtendTools()
     {
-         string prof(int value)
+        string ProFormat(int value)
         {
-            return $"{ value / 100}%";
+            return $"{value / 100}%";
         }
 
-         string speedf(int value)
+        string SpeedFormat(int value)
         {
-            return $"{ value / 100}m/s";
+            return $"{value / 100}m/s";
         }
 
-         string disf(int value)
+        string DisFormat(int value)
         {
-            return $"{ value / 100}m";
+            return $"{value / 100}m";
         }
 
-         string deff(int value)
+        string ValueFormat(int value)
         {
-            return $"{ value }";
+            return $"{value}";
         }
-         string tf(int value)
-        {
-            return $"{ value / 1000}s";
-        }
-         string at(int value)
+
+        string AttackFormat(int value)
         {
             return $"{value / 1000f}t/s";
         }
 
-        ValueFormat.Add(HeroPropertyType.CastSpeed, tf);
-        ValueFormat.Add(HeroPropertyType.Crt, prof);
-        ValueFormat.Add(HeroPropertyType.CrtDamageRate, prof);
-        ValueFormat.Add(HeroPropertyType.Damage, deff);
-        ValueFormat.Add(HeroPropertyType.Defance, deff);
-        ValueFormat.Add(HeroPropertyType.Dodge, prof);
-        ValueFormat.Add(HeroPropertyType.Hit, prof);
-        ValueFormat.Add(HeroPropertyType.HitBack, prof);
-        ValueFormat.Add(HeroPropertyType.HitBurn, prof);
-        ValueFormat.Add(HeroPropertyType.HitFreeze, prof);
-        ValueFormat.Add(HeroPropertyType.HitImmob, prof);
-        ValueFormat.Add(HeroPropertyType.Hpdrain, prof);
-        ValueFormat.Add(HeroPropertyType.AttackSpeed, at);
-        ValueFormat.Add(HeroPropertyType.MaxHp, deff);
-        ValueFormat.Add(HeroPropertyType.MaxMp, deff);
-        ValueFormat.Add(HeroPropertyType.MoveSpeed, speedf);
-        ValueFormat.Add(HeroPropertyType.MpDrain, prof);
-        ValueFormat.Add(HeroPropertyType.ViewDistance, disf);
-        ValueFormat.Add(HeroPropertyType.VsBoss, prof);
-        ValueFormat.Add(HeroPropertyType.VsElite, prof);
+        //ValueFormat.Add(HeroPropertyType.CastSpeed, tf);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.Crt, ProFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.CrtDamageRate, ProFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.Damage, ValueFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.Defance, ValueFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.Dodge, ProFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.Hit, ProFormat);
+        //ValueFormat.Add(HeroPropertyType.HitBack, prof);
+        //ValueFormat.Add(HeroPropertyType.HitBurn, prof);
+        //ValueFormat.Add(HeroPropertyType.HitFreeze, prof);
+        //ValueFormat.Add(HeroPropertyType.HitImmob, prof);
+        //ValueFormat.Add(HeroPropertyType.Hpdrain, prof);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.AttackSpeed, AttackFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.MaxHp, ValueFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.MaxMp, ValueFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.MoveSpeed, SpeedFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.MpDrain, ProFormat);
+        ExtendTools.ValueFormat.Add(HeroPropertyType.ViewDistance, DisFormat);
+        //ValueFormat.Add(HeroPropertyType.VsBoss, prof);
+        // ValueFormat.Add(HeroPropertyType.VsElite, prof);
 
     }
 
